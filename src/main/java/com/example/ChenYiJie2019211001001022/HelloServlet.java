1a2c4e6acbaa97005339591a4e97c1903d911572
2019211001001022-ChenYijie <3302131288@qq.com>
2021-05-08 09:22:41 +0800

    HW-Week 9  Commit

--- src/main/java/com/example/ChenYiJie2019211001001022/HelloServlet.java
+++ src/main/java/com/example/ChenYiJie2019211001001022/HelloServlet.java
@@ -1,10 +1,13 @@
 package com.example.ChenYiJie2019211001001022;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -14,6 +17,7 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        System.out.println("i am in HelloServlet-->doGet()");
 
         // Hello
         PrintWriter out = response.getWriter();
